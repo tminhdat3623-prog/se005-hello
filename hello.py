c0766675c15515c47c757de5b1a66824497da0a5
“265208840-uit” <26520884@gm.uit.edu.vn>
2026-09-14 22:21:16 +0700

    Add a team message - NGUYEN TRI KHANH

--- hello.py
+++ hello.py
@@ -1,3 +1,6 @@
 print("Hello World from TRAN MINH DAT")
 print("Welcome to SE005")
 print("Oh My God Dude")
+for i in range(0,5):
+	print(" ")
+print("New teammate joined! - NGUYEN TRI KHANH 26520884")
